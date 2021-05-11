aecd3bdb993b7323d9fd6b065fe90738cef1bb32
oussama berhayla <maoussaberhayla@gmail.com>
2021-05-11 11:46:25 +0000

    d

--- lara2/app/Http/Controllers/coption.php
+++ lara2/app/Http/Controllers/coption.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use App\Models\moption;
+use Symfony\Component\HttpFoundation\Request;
+
 
 
 class coption extends Controller
